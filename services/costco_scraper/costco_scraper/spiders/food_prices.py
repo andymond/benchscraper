108ef55432021028c501258cb6d597491b3b862e
Andrew Dymond <andy.dymond@gmail.com>
2018-06-02 19:36:49 -0600

    Add scrapy-rotating-proxies and proxy list

--- services/costco_scraper/costco_scraper/spiders/food_prices.py
+++ services/costco_scraper/costco_scraper/spiders/food_prices.py
@@ -4,6 +4,11 @@ class FoodPricesSpider(scrapy.Spider):
     name = "food_prices"
     urls = ['https://www.costco.com/grocery-household.html?dept=Grocery&keyword=beans']
 
+    def response_is_ban(self, request, response):
+        return b'banned' in response.body
+
+    def exception_is_ban(self, request, exception):
+        return None
 
     def start_requests(self):
         for url in self.urls:
@@ -13,7 +18,7 @@ class FoodPricesSpider(scrapy.Spider):
         names = response.xpath('//p[@class="description"]/a/text()').extract()
         prices = response.xpath('//div[@class="price"]/text()').extract()
         associated = list(zip(names, prices))
-        data = [{'name': name, 'price': price, 'seller': 'costco'}, for i, (name, price) in enumerate(associated)]
+        data = [{'name': name, 'price': price, 'seller': 'costco'} for i, (name, price) in enumerate(associated)]
 
         # yield {
         #     'name': response.xpath('//div[@class="price"]/text()').extract(),
