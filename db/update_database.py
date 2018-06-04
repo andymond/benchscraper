541093b7f6cfca960dfa06ecf82b6f95d3e1fec1
Andrew Dymond <andy.dymond@gmail.com>
2018-06-04 17:50:24 -0600

    Add db/update_database.py

---
+++ db/update_database.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import os
+import json
+
+with open('./data/amazon_results.json') as file:
+    amazon_results = json.load(file)
+
+with open('./data/costco_results.json') as file:
+    costco_results = json.load(file)
+
+with open('./data/walmart_api_results.json') as file:
+    walmart_results = json.load(file)
+
+data_list = []
+data_list.extend(amazon_results)
+data_list.extend(costco_results)
+data_list.extend(walmart_results)
+
+if 'PROD' in os.environ:
+    uri = os.environ['PROD']
+    client = MongoClient(uri)
+    db = client.get_default_database()
+    print(db.collection_names())
+else:
+    client = MongoClient()
+    db = client.benchscraper_dev
+
+items = db.items
+items.remove({})
+items.insert_many(data_list)
