fac644811f75ef318a697bb8e8325eb80fa2e7c2
Andrew Dymond <andy.dymond@gmail.com>
2018-06-04 13:15:02 -0600

    Update __init__ file in responses to generate HtmlResponse instead of Response

--- test/responses/__init__.py
+++ test/responses/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from scrapy.http import Response, Request
+from scrapy.http import HtmlResponse, Request
 
 def fake_response_from_file(file_name, url=None):
     if not url:
@@ -12,9 +12,8 @@ def fake_response_from_file(file_name, url=None):
         responses_dir = os.path.dirname((os.path.realpath(__file__)))
         file_path = os.path.join(responses_dir + file_name)
     else:
-        file_path = file_name
+        file_path = 'responses' + file_name
 
-    file_content = open(file_path, 'r').read()
-    response = Response(url=url, request=request, body=file_content)
-    response.encoding = 'utf-8'
+    file_content = open(file_path, 'r', encoding='utf-8').read()
+    response = HtmlResponse(url=url, request=request, body=file_content, encoding='utf-8')
     return response
