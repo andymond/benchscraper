b8b35b55095262984420c97bb1fb3c6c37d7e2d5
Andrew Dymond <andy.dymond@gmail.com>
2018-06-04 15:07:35 -0600

    Add run_all.py to run all tests

---
+++ test/run_all.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+
+def create_suite():
+    suite = unittest.TestSuite()
+    for file in os.listdir("."):
+        if file.endswith("_test.py"):
+            print("running " + file)
+            name = file[0:-3]
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromName(name))
+    return suite
+
+suite = create_suite()
+unittest.TextTestRunner().run(suite)
