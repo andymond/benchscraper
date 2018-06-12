4b09345e7968965ccb5fdaa7e8e72631bda6c92e
Andrew Dymond <andy.dymond@gmail.com>
2018-06-12 14:50:41 -0600

    Put background-worker files in subdirectory

--- background-workers/jobs.py
+++ background-workers/jobs.py
@@ -5,8 +5,8 @@ import amazon_spider
 import costco_spider
 sys.path.insert(0, './services')
 import walmart_api_recorder
-# sys.path.insert(0, './db')
-# import update_database
+sys.path.insert(0, './db')
+import update_database
 
 def get_walmart():
     os.system('printf "gathering walmart data...\n"')
@@ -20,6 +20,9 @@ def get_costco():
 def send_to_database():
     os.system('printf "sending data to db...\n"')
 
+get_walmart()
+get_amazon()
+get_costco()
 send_to_database()
 
 #
